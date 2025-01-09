19f567974b16e2eca749b5f42c1aae5c2fae90d6
Hanschase <101171938+Hanschase@users.noreply.github.com>
2025-01-09 11:00:42 +0800

    Add files via upload

---
+++ forward_message.py
@@ -0,0 +1,65 @@
+import json
+import aiohttp
+import os
+
+class forward_message():
+    def __init__(self, host:str, port: int):
+        self.url = f"http://{host}:{port}"
+
+    async def send(self, group_id:str, img_info:list):
+        image = self.get_media_path("temp.jpg")
+        message_data = {
+            "group_id": group_id,
+            "user_id": "1900487324",
+            "messages": [
+                {
+                    "type": "node",
+                    "data": {
+                        "user_id": "3870128501",
+                        "nickname": "BOT",
+                        "content": [
+                            {
+                                "type": "text",
+                                "data": {
+                                    "text": f"pid:{img_info[0]}\ntitle:{img_info[1]}\nauthor:{img_info[2]}"
+                                }
+                            },
+                            {
+                                "type": "image",
+                                "data": {
+                                    "file": image,
+                                }
+                            }
+                        ]
+                    }
+                }
+            ],
+            "news": [
+                {
+                    "text": f"pid:{img_info[0]}"
+                }
+            ],
+            "prompt": "冲就完事儿了",
+            "summary": "一天就知道冲冲冲",
+            "source": img_info[1]
+        }
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        payload = json.dumps(message_data)
+        async with aiohttp.ClientSession(self.url, headers=headers) as session:
+            async with session.post("/send_forward_msg", data=payload) as response:
+                await response.json()
+                print(response)
+
+    def get_media_path(self, media_path):
+        """
+        获取媒体的本地绝对路径或网络路径
+        """
+        if media_path:
+            if media_path.startswith('http'):
+                return media_path
+            elif os.path.isfile(media_path):
+                abspath = os.path.abspath(os.path.join(os.getcwd(), media_path)).replace('\\', '\\\\')
+                return f"file:///{abspath}"
+        return ''
--- main.py
+++ main.py
@@ -1,48 +1,60 @@
+from mirai import MessageChain
 from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
 from pkg.plugin.events import *  # 导入事件类
-
+from pkg.platform.types import *
+from plugins.GiveMeSetuPlugin.get_image import get_image
+import re
+from plugins.GiveMeSetuPlugin.forward_message import forward_message
 
 # 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
+@register(name="GiveMeSetuPlugin", description="功能正如其名，让她给，她不得不给！", version="0.1", author="Hanschase")
 class MyPlugin(BasePlugin):
 
     # 插件加载时触发
     def __init__(self, host: APIHost):
-        pass
+        self.forward_message = forward_message(host="127.0.0.1", port=3000)
+
 
     # 异步初始化
     async def initialize(self):
         pass
 
-    # 当收到个人消息时触发
-    @handler(PersonNormalMessageReceived)
-    async def person_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
-
-            # 阻止该事件默认行为（向接口获取回复）
+    @handler(PersonMessageReceived)
+    @handler(GroupMessageReceived)
+    async def message_received(self, ctx: EventContext):
+        msg = str(ctx.event.message_chain).strip()
+        #msg = ctx.event.text_message.strip()
+        if re.search(r'setu|涩图|色图', msg, re.IGNORECASE):
             ctx.prevent_default()
-
-    # 当收到群消息时触发
-    @handler(GroupNormalMessageReceived)
-    async def group_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, everyone!"
-            ctx.add_return("reply", ["hello, everyone!"])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
+            msg = msg.split(" ")
+            if len(msg)==1 or len(msg)>2:
+                keyword = ""
+            else:
+                keyword = msg[1]
+            r18_flag = 1 if 'r' in msg[0].lower() else 0
+            self.ap.logger.info(f"接收到关键词{keyword}")
+            loop_flag = 3# 重定向次数
+            while loop_flag:
+                try:
+                    img_info = await get_image(keyword, r18_flag)
+                    self.ap.logger.info(f"Image info：{img_info}")
+                    loop_flag = 0
+                except Exception as e:
+                    print(str(e))
+                    if str(e) == "404":
+                        self.ap.logger.error("未查询到图片，正在进行重定向")
+                        loop_flag -= 1
+                    else:
+                        await ctx.send_message(ctx.event.launcher_type, str(ctx.event.launcher_id),MessageChain([f"发生了一个错误：{e}"]))
+                        self.ap.logger.error(e)
+                        return
+            self.ap.logger.info(f"正在发送图片...")
+            #await ctx.send_message(ctx.event.launcher_type, str(ctx.event.launcher_id),MessageChain(["已获取图片，正在发送...（没发送出来就是被风控了）"]))
+            # await ctx.send_message(ctx.event.launcher_type, str(ctx.event.launcher_id), MessageChain([f"pid:{img_info[0]}\n",
+            #                                                                                     f"title:{img_info[1]}\n",
+            #                                                                                     f"author:{img_info[2]}\n",
+            #                                                                                    Image(path="temp.png")]))
+            await self.forward_message.send(str(ctx.event.launcher_id), img_info)
 
     # 插件卸载时触发
     def __del__(self):
